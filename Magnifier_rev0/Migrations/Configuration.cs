30044548ddb507ef59a790522eb38b6841e99c60
Mieke van Arkel <arkelmieke@gmail.com>
2021-04-16 22:06:28 +0400

    initial commit

---
+++ Magnifier_rev0/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace Magnifier_rev0.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Magnifier_rev0.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Magnifier_rev0.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(Magnifier_rev0.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
